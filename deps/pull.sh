ac110bde679f0ef3e174bd36420de789adcc1943
Zachary Hensley <zhensley@integralads.com>
2023-11-09 10:50:18 -0600

    More script updates

---
+++ deps/pull.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+pushd ${SCRIPT_DIR}
+
+yes Y | rm -r spdlog && yes Y | rm -r boringssl
+
+# reqs: cmake, Go
+git config --global http.postBuffer 1048576000
+git config --global https.postBuffer 1048576000
+git clone -c http.sslverify=false https://boringssl.googlesource.com/boringssl --depth 1
+git clone https://github.com/gabime/spdlog.git --depth 1
+
+popd
