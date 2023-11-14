5628a416bd4a873ebd3637ef940ab447e0c008d6
Zachary Hensley <zhensley@integralads.com>
2023-11-14 00:59:45 -0600

    Add token input to launching chrome

--- scripts/launch_chrome_with_trusttokens.sh
+++ scripts/launch_chrome_with_trusttokens.sh
@@ -2,8 +2,15 @@
 
 # https://private-state-token-demo.glitch.me/
 
+# the token here is the pubkey of the issued trust token
+default_token="AAAAAQQ7W5gOubJT3kTpzNGsekT9RZPXgXGrOMB2+QPw/ZzAuLrM3kc8eyHuTc1KmKjH4sh5+ev5GCI4HVVd46o6rWvNvk0iZQtVuUPhT8X54Ajebng8v5zUnpnPuTjGqlc7+MM="
+
+if [ "$1" != "" ]; then
+  default_token="$1"
+fi
+
 open -a Google\ Chrome \
   --args \
   --enable-blink-features=PrivateStateTokens,PrivateStateTokensAlwaysAllowIssuance,PrivacySandboxSettings3 \
-  --additional-private-state-token-key-commitments='{ "https://trust-token-server.com": { "PrivateStateTokenV1VOPRF": { "protocol_version": "PrivateStateTokenV1VOPRF", "id": 1, "batchsize": 1, "keys": { "1": { "Y": "AAAAAQQ7W5gOubJT3kTpzNGsekT9RZPXgXGrOMB2+QPw/ZzAuLrM3kc8eyHuTc1KmKjH4sh5+ev5GCI4HVVd46o6rWvNvk0iZQtVuUPhT8X54Ajebng8v5zUnpnPuTjGqlc7+MM=", "expiry": "1715356984440000" } } } } }'
+  --additional-private-state-token-key-commitments='{ "https://trust-token-server.com": { "PrivateStateTokenV1VOPRF": { "protocol_version": "PrivateStateTokenV1VOPRF", "id": 1, "batchsize": 1, "keys": { "1": { "Y": "'$default_token'", "expiry": "1715356984440000" } } } } }'
 
